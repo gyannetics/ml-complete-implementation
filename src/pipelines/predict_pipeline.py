bfe31862159ea2f966e0cb045c1f0a88a9450a76
Santosh KV <theaimonk24@gmail.com>
2023-12-06 16:21:05 -0600

    updated model_path and preprocessor_path in predict_pipeline module

--- src/pipelines/predict_pipeline.py
+++ src/pipelines/predict_pipeline.py
@@ -5,23 +5,25 @@ from src.utils import load_object
 import os
 from src.logger import logging
 
+
 class PredictPipeline:
     def __init__(self):
         pass
-    
+
     def predict(self, features):
         try:
-            model_path = 'artifacts\model.pkl'
-            preprocessor_path = 'artifacts\preprocessor.pkl'
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
             return preds
-        
+
         except Exception as e:
             raise CustomException(e, sys)
-        
+
+
 class CustomData:
     """
     A class to encapsulate input data for prediction.
