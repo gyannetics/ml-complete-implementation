4162067f31137fafd8b059d93173c84bcc7d5761
Santosh KV <theaimonk24@gmail.com>
2023-12-06 15:06:34 -0600

    updated application.py

--- application.py
+++ application.py
@@ -6,15 +6,15 @@ from src.pipelines.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 
-app = application
+# app = application
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@application.route('/predict', methods=['GET', 'POST'])
 def predict_data():
     if request.method == 'GET':
         return render_template('home.html')
